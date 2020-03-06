ad01f8cca4bfd82b0c686a3918dcb6c505ae317a
Andreas Oxinos <oxinosa@gmail.com>
2020-03-06 12:22:30 +0100

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+server = app.listen(3001, function() {
+  console.log("server is running on port 3001");
+});
+
+const io = require("socket.io")(server);
+
+io.on("connection", function(socket) {
+  console.log(socket.id);
+});
